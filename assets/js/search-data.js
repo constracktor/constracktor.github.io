64d0e894978e0f43ebbfc71b5b94ee5edc819f54
constracktor <74077030+constracktor@users.noreply.github.com>
2025-12-30 21:39:47 +0000

    Deploying to gh-pages from @ constracktor/constracktor.github.io@f34d92046145ec739a9a1c7dab260d477c4b6b13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-talks",
           title: "Talks",
-          description: "Given talks in reversed chronological order.",
+          description: "Given talks in reversed chronological order. Coming soon ...",
           section: "Navigation",
           handler: () => {
             window.location.href = "/talks/";
